9c49d0325ec3c89133e228aee90ed3e9e486ee24
AppCan <guangqiao.li@3g2win.com>
2015-01-07 18:22:04 +0800

    Initial
    Initial

---
+++ EUExClipboard/EUExClipboard/EUExClipboard.h
@@ -0,0 +1,16 @@
+//
+//  EUExClipBoard.h
+//  AppCan
+//
+//  Created by AppCan on 12-5-16.
+//  Copyright 2012 AppCan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "EUExBase.h"
+
+@interface EUExClipboard : EUExBase {
+
+}
+
+@end
